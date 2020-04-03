b0fcdaf98bdc9aea69efe1cd488294121540e25f
dom-codex <dominicwest7@gmail.com>
2020-04-03 11:30:32 +0100

    add javascript, whatsapp share and some image assets

--- js/products.js
+++ js/products.js
@@ -3,14 +3,14 @@ $('#contact-link').on('click',()=>{
     $('#myModal').slideDown(2000)
     $('.modal-content').animate({
         "opacity":"1"
+    },3500);
+})
+$('#btn-cont button').on('click',()=>{
+    $('#myModal').slideDown(2000)
+    $('.modal-content').animate({
+        "opacity":"1"
     },3500)
 })
 $('.close').on('click',()=>{
     $('#myModal').css('display','none');
 })
-$('#btn-cont button').on('click',()=>{
-    $('#myModal').slideDown(2000);
-    $('.modal-content').animate({
-        "opacity":"1"
-    },3500) ;             
- });
